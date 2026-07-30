b4864b78f09d16f5f3340c9218256de9e4b11596
Marcos Ferreira <marcosferreira.developer@gmail.com>
2026-07-30 18:52:44 -0300

    build: upgrade Tailwind CSS from v3 standalone binary to v4 npm CLI

--- tailwindcss-build.sh
+++ tailwindcss-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tailwindcss-linux-x64 -i ./css/tailwind/input.css -o ./css/tailwind/output.css --minify
+npx @tailwindcss/cli -i ./css/tailwind/input.css -o ./css/tailwind/output.css --minify
